ad6464dcb3f8dcc26cbd5eb417ccc4b18af9ee91
Ubuntu <ubuntu@ip-172-31-13-124.us-east-2.compute.internal>
2021-08-31 23:13:57 +0000

    first comment

---
+++ SimpleCRUD+2/SimpleCRUD/config.js
@@ -0,0 +1,11 @@
+var config = {}
+
+//DB Details
+config.DBNAME = 'express'
+config.DBUSERNAME = 'tony'
+config.DBPASSWORD = 'atom12'
+config.DBDIALECT = 'postgres'
+config.DBPORT = 5432
+config.DBREGION = 'us-east-1'
+config.DBHOST = 'localhost'
+module.exports = config
